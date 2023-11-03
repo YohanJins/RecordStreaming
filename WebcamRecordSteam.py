38cdce36b27f331b433fd61af7a00102495e74a3
Yohan <yohanjin0925@gmail.com>
2023-11-03 01:33:57 -0400

    Comment modified

--- WebcamRecordSteam.py
+++ WebcamRecordSteam.py
@@ -1,15 +1,18 @@
 import cv2
 import os
 
+# Initialize the webcam capture
 cap = cv2.VideoCapture(0)
 print('width :%d, height : %d' % (cap.get(3), cap.get(4)))
 
+# Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 out = None
 recording = False
 
 def get_next_filename():
-    """현재 디렉토리에 있는 파일 중 recorded_video[i].avi 형식을 가진 마지막 파일의 인덱스를 찾아서 다음 인덱스를 반환합니다."""
+    """Find the index of the last file in the current directory with the format 
+    recorded_video[i].avi and return the next index."""
     index = 0
     while True:
         filename = f"recorded_video_{index}.avi"
@@ -17,43 +20,52 @@ def get_next_filename():
             return filename
         index += 1
 
+# Main loop for capturing webcam frames
 while True:
     ret, frame = cap.read()
     
+    # Break the loop if frame is not received
     if not ret:
         break
 
-    # 녹화 중이라면 "Now recording" 텍스트 추가
+    # If recording, add "Now recording" text to the frame
     if recording:
         cv2.putText(frame, "Now recording", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
+    # Display the resulting frame
     cv2.imshow('webcam', frame)
 
+    # Capture keypresses
     key = cv2.waitKey(1)
 
-    # 'r'키를 누르면 녹화 시작
+    # If 'r' key is pressed, start recording
     if key == ord('r') and not recording:
         next_filename = get_next_filename()
+        # Create a VideoWriter object to record video
         out = cv2.VideoWriter(next_filename, fourcc, 25.0, (int(cap.get(3)), int(cap.get(4))))
         recording = True
         print(f"Recording started... Saving to {next_filename}")
 
-    # 's'키를 누르면 녹화 중지
+    # If 's' key is pressed, stop recording
     elif key == ord('s') and recording:
         recording = False
+        # Release the VideoWriter object
         out.release()
         print("Recording stopped.")
 
-    # 'q'키를 누르면 웹캠 영상 중지, 만약 녹화 중이었다면 녹화도 중지
+    # If 'q' key is pressed, exit the program
     elif key == ord('q'):
         if recording:
             recording = False
+            # Ensure the recording is properly stopped before exiting
             out.release()
             print("Recording stopped.")
         break
 
+    # Write the frame to the video file if recording
     if recording:
         out.write(frame)
 
+# Release the webcam capture and close all windows
 cap.release()
 cv2.destroyAllWindows()
